bf6d63b7d64cba723833a5ca8fd1ab170104b4e0
valyala <valyala@6b04b937-504c-0410-a5dc-c58d0effbdb5>
2008-05-07 15:40:29 +0000

    now blocking_queue has maximum size

--- trunk/include/public/ff_blocking_queue.h
+++ trunk/include/public/ff_blocking_queue.h
@@ -7,7 +7,7 @@ extern "C" {
 
 struct ff_blocking_queue;
 
-struct ff_blocking_queue *ff_blocking_queue_create();
+struct ff_blocking_queue *ff_blocking_queue_create(int max_size);
 
 void ff_blocking_queue_delete(struct ff_blocking_queue *queue);
 
