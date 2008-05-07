fbd5f7680793141ff648314330ab3e359f5dfec6
valyala <valyala@6b04b937-504c-0410-a5dc-c58d0effbdb5>
2008-05-07 10:06:55 +0000

    added error checking macros for fatal (unrecoverable) winapi and winsock errors

---
+++ src/arch/win/ff_win_error_check.h
@@ -0,0 +1,16 @@
+#ifndef FF_WIN_ERROR_CHECK
+#define FF_WIN_ERROR_CHECK
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define ff_winapi_fatal_error_check(expression, format, ...)
+
+#define ff_winsock_fatal_error_check(expression, format, ...)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
