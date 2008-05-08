19a2b600abdd5da95190ef14fe6e792e8610f344
valyala <valyala@6b04b937-504c-0410-a5dc-c58d0effbdb5>
2008-05-08 18:17:22 +0000

    ff_arch_net_addr structure was extracted from ff_arch_tcp files

---
+++ trunk/src/arch/win/ff_arch_net_addr.c
@@ -0,0 +1,82 @@
+#include "ff_win_stdafx.h"
+
+#include "private/arch/ff_arch_net_addr.h"
+#include "private/ff_core.h"
+#include "ff_win_net_addr.h"
+
+#define MAX_STR_PORT_SIZE 6
+
+struct threadpool_addr_resolve_data
+{
+	struct ff_arch_net_addr *addr;
+	const wchar_t *host;
+	int port;
+	int is_success;
+};
+
+static int resolve_addr(struct ff_arch_net_addr *addr, const wchar_t *host, int port)
+{
+	int is_success = 0;
+	int rv;
+	ADDRINFOW hint;
+	PADDRINFOW addr_info_ptr;
+	wchar_t str_port[MAX_STR_PORT_SIZE];
+
+	ff_assert(port >= 0);
+	ff_assert(port < 0x10000);
+
+	memset(&hint, 0, sizeof(hint));
+	hint.ai_family = AF_INET;
+
+	rv = swprintf_s(str_port, MAX_STR_PORT_SIZE, L"%d", port);
+	ff_assert(rv != -1);
+	addr_info_ptr = NULL;
+	rv = GetAddrInfoW(host, str_port, &hint, &addr_info_ptr);
+	if (rv != 0)
+	{
+		goto end;
+	}
+	ff_assert(addr_info_ptr != NULL);
+
+	ff_assert(addr_info_ptr->ai_addrlen == sizeof(addr->addr));
+	memcpy(&addr->addr, addr_info_ptr->ai_addr, sizeof(addr->addr));
+	FreeAddrInfoW(addr_info_ptr);
+	is_success = 1;
+
+end:
+	return is_success;
+}
+
+static void threadpool_addr_resolve_func(void *ctx)
+{
+	struct threadpool_addr_resolve_data *data;
+
+	data = (struct threadpool_addr_resolve_data *) ctx;
+	data->is_success = resolve_addr(data->addr, data->host, data->port);
+}
+
+struct ff_arch_net_addr *ff_arch_net_addr_create()
+{
+	struct ff_arch_net_addr *addr;
+
+	addr = (struct ff_arch_net_addr *) ff_malloc(sizeof(*addr));
+		
+	return addr;
+}
+
+void ff_arch_net_addr_delete(struct ff_arch_net_addr *addr)
+{
+	ff_free(addr);
+}
+
+int ff_arch_net_addr_resolve(struct ff_arch_net_addr *addr, const wchar_t *host, int port)
+{
+	struct threadpool_addr_resolve_data data;
+	data.addr = addr;
+	data.host = host;
+	data.port = port;
+	data.is_success = 0;
+	ff_core_threadpool_execute(threadpool_addr_resolve_func, &data);
+
+	return data.is_success;
+}
--- trunk/src/arch/win/ff_win_stdafx.h
+++ trunk/src/arch/win/ff_win_stdafx.h
@@ -2,12 +2,17 @@
 #define FF_WIN_STDAFX
 
 #include "private/ff_common.h"
+#include "ff_win_error_check.h"
 
 #define _WIN32_WINNT 0x0502
 
 #define WIN32_LEAN_AND_MEAN
 
 #include <windows.h>
+#include <winsock2.h>
+#include <Mswsock.h>
+#include <ws2tcpip.h>
+
 #include <string.h>
 #include <stdio.h>
 #include <wchar.h>
