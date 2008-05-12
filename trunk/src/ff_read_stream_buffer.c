fc4a55976f74c99c7ececb6501d80fa4ee94b671
valyala <valyala@6b04b937-504c-0410-a5dc-c58d0effbdb5>
2008-05-12 08:06:39 +0000

    added stream buffer for read operations. This buffer allows to minimize the number of read() calls for the stream

---
+++ trunk/src/ff_read_stream_buffer.c
@@ -0,0 +1,104 @@
+#include "private/ff_common.h"
+
+#include "private/ff_read_stream_buffer.h"
+
+struct ff_read_stream_buffer
+{
+	ff_read_stream_func read_func;
+	void *func_ctx;
+	char *buf;
+	int capacity;
+	int size;
+	int start_pos;
+};
+
+struct ff_read_stream_buffer *ff_read_stream_buffer_create(ff_read_stream_func read_func, void *func_ctx, int capacity)
+{
+	struct ff_read_stream_buffer *buffer;
+
+	ff_assert(capacity > 0);
+
+	buffer = (struct ff_read_stream_buffer *) ff_malloc(sizeof(*buffer));
+	buffer->read_func = read_func;
+	buffer->func_ctx = func_ctx;
+	buffer->buf = (char *) ff_malloc(capacity);
+	buffer->capacity = capacity;
+	buffer->size = 0;
+	buffer->start_pos = 0;
+
+	return buffer;
+}
+
+void ff_read_stream_buffer_delete(struct ff_read_stream_buffer *buffer)
+{
+	ff_free(buffer->buf);
+	ff_free(buffer);
+}
+
+int ff_read_stream_buffer_read(struct ff_read_stream_buffer *buffer, void *buf, int len)
+{
+	char *char_buf = (char *) buf;
+	int total_bytes_read = 0;
+
+	for (;;)
+	{
+		int bytes_read;
+
+		if (buffer->size == 0)
+		{
+			while (len >= buffer->capacity)
+			{
+				bytes_read = buffer->read_func(buffer->func_ctx, char_buf, len);
+				if (bytes_read == -1)
+				{
+					total_bytes_read = -1;
+					goto end;
+				}
+				if (bytes_read == 0)
+				{
+					goto end;
+				}
+
+				char_buf += bytes_read;
+				total_bytes_read += bytes_read;
+				len -= bytes_read;
+			}
+			if (len == 0)
+			{
+				goto end;
+			}
+
+			bytes_read = buffer->read_func(buffer->func_ctx, buffer->buf, buffer->capacity);
+			if (bytes_read == -1)
+			{
+				total_bytes_read = -1;
+				goto end;
+			}
+			if (bytes_read == 0)
+			{
+				goto end;
+			}
+
+			buffer->size = bytes_read;
+			buffer->start_pos = 0;
+		}
+
+		bytes_read = len > buffer->size ? buffer->size : len;
+		if (bytes_read == 0)
+		{
+			goto end;
+		}
+
+		memcpy(char_buf, buffer->buf + buffer->start_pos, bytes_read);
+
+		buffer->start_pos += bytes_read;
+		buffer->size -= bytes_read;
+
+		char_buf += bytes_read;
+		total_bytes_read += bytes_read;
+		len -= bytes_read;
+	}
+
+end:
+	return total_bytes_read;
+}
