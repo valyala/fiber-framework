72526a8e927cdbe6dc37a6c9db57ae4f5f78a968
valyala <valyala@6b04b937-504c-0410-a5dc-c58d0effbdb5>
2008-07-09 10:38:54 +0000

    updated rpc_serialization: changed format of integers encoding - new format requires less code, but more CPU time than an old one; added prototype functions for blob serialization (the main aim of blob is to effectively store very large byte arrays)

--- src/rpc_serialization.c
+++ src/rpc_serialization.c
@@ -4,54 +4,106 @@ struct rpc_stream;
 
 int rpc_stream_read(struct rpc_stream *stream, void *buf, int len);
 int rpc_stream_write(struct rpc_stream *stream, const void *buf, int len);
+void rpc_stream_close(struct rpc_stream *stream);
 
-#define U_FITS_IN_OCTETS(data, size) (((data) >> (7 * (size))) == 0)
-#define S_FITS_IN_OCTETS(data, size) (((data) >> (7 * (size) + 1)) == 0)
+#define BUF_SIZE 0x10000
 
-#define MAX_BYTES_SIZE 0x10000
+int rpc_stream_copy(struct rpc_stream *dst_stream, struct rpc_stream *src_stream, int len)
+{
+	int bytes_copied;
+	uint8_t *buf;
+
+	ff_assert(len >= 0);
+
+	buf = (uint8_t *) ff_malloc(BUF_SIZE);
+	bytes_copied = 0;
+	while (len > 0)
+	{
+		int bytes_read;
+		int bytes_written;
+		int bytes_to_read;
+
+		bytes_to_read = len > BUF_SIZE ? BUF_SIZE : len;
+
+		bytes_read = rpc_stream_read(src_stream, buf, bytes_to_read);
+		if (bytes_read != bytes_to_read)
+		{
+			bytes_copied = -1;
+			break;
+		}
+
+		bytes_written = rpc_stream_write(dst_stream, buf, bytes_read);
+		if (bytes_written != bytes_read)
+		{
+			bytes_copied = -1;
+			break;
+		}
+
+		bytes_copied += bytes_written;
+		len -= bytes_written;
+	}
+
+	ff_free(buf);
+
+	return bytes_copied;
+}
+
 #define MAX_STRING_SIZE 0x10000
+
+#define BITS_PER_OCTET 7
+#define MAX_UINT_N_OCTETS(n) (((n) + BITS_PER_OCTET - 1) / BITS_PER_OCTET)
+#define MAX_UINT32_OCTETS MAX_UINT_N_OCTETS(32)
+#define MAX_UINT64_OCTETS MAX_UINT_N_OCTETS(64)
+#define OCTET_MASK ((1 << BITS_PER_OCTET) - 1)
 
 int uint32_serialize(uint32_t data, struct rpc_stream *stream)
 {
 	int bytes_written;
 	int len;
-	uint8_t buf[5];
+	uint8_t buf[MAX_UINT32_OCTETS];
 
-	if (U_FITS_IN_OCTETS(data, 1))
+	len = 0;
+	do
 	{
-		len = 1;
-		buf[0] = (uint8_t) data;
+		uint8_t octet;
+		
+		ff_assert(len < MAX_UINT32_OCTETS);
+		octet = (uint8_t) (data & OCTET_MASK);
+		data >>= BITS_PER_OCTET;
+		octet |= (uint8_t) ((data != 0) << BITS_PER_OCTET);
+		buf[len] = octet;
+		len++;
 	}
-	else if (U_FITS_IN_OCTETS(data, 2))
+	while (data != 0);
+
+	bytes_written = rpc_stream_write(stream, buf, len);
+	if (bytes_written != len)
 	{
-		len = 2;
-		buf[0] = (uint8_t) (0x80 | (data >> 8));
-		buf[1] = (uint8_t) data;
+		bytes_written = -1;
 	}
-	else if (U_FITS_IN_OCTETS(data, 3))
+
+	return bytes_written;
+}
+
+int uint64_serialize(uint64_t data, struct rpc_stream *stream)
+{
+	int bytes_written;
+	int len;
+	uint8_t buf[MAX_UINT64_OCTETS];
+
+	len = 0;
+	do
 	{
-		len = 3;
-		buf[0] = (uint8_t) (0xC0 | (data >> 16));
-		buf[1] = (uint8_t) (data >> 8);
-		buf[2] = (uint8_t) data;
+		uint8_t octet;
+		
+		ff_assert(len < MAX_UINT64_OCTETS);
+		octet = (uint8_t) (data & OCTET_MASK);
+		data >>= BITS_PER_OCTET;
+		octet |= (uint8_t) ((data != 0) << BITS_PER_OCTET);
+		buf[len] = octet;
+		len++;
 	}
-	else if (U_FITS_IN_OCTETS(data, 4))
-	{
-		len = 4;
-		buf[0] = (uint8_t) (0xE0 | (data >> 24));
-		buf[1] = (uint8_t) (data >> 16);
-		buf[2] = (uint8_t) (data >> 8);
-		buf[3] = (uint8_t) data;
-	}
-	else
-	{
-		len = 5;
-		buf[0] = 0xF0;
-		buf[1] = (uint8_t) (data >> 24);
-		buf[2] = (uint8_t) (data >> 16);
-		buf[3] = (uint8_t) (data >> 8);
-		buf[4] = (uint8_t) data;
-	}
+	while (data != 0);
 
 	bytes_written = rpc_stream_write(stream, buf, len);
 	if (bytes_written != len)
@@ -64,312 +116,131 @@ int uint32_serialize(uint32_t data, struct rpc_stream *stream)
 
 int uint32_unserialize(uint32_t *data, struct rpc_stream *stream)
 {
-	int total_bytes_read = -1;
 	int bytes_read;
-	uint8_t buf[5];
 	uint32_t u_data;
+	uint8_t octet;
 
-	bytes_read = rpc_stream_read(stream, buf, 1);
-	if (bytes_read != 1)
+	u_data = 0;
+	bytes_read = 0;
+	do
 	{
-		goto end;
-	}
+		int len;
 
-	if ((buf[0] & 0x80) == 0x00)
-	{
-		u_data = (uint32_t) buf[0];
-	}
-	else if ((buf[0] & 0xC0) == 0x80)
-	{
-		bytes_read = rpc_stream_read(stream, buf + 1, 1);
-		if (bytes_read != 1)
+		if (bytes_read >= MAX_UINT32_OCTETS)
 		{
+			bytes_read = -1;
 			goto end;
 		}
-		u_data = (((uint32_t) (buf[0] & 0x3F)) << 8) || ((uint32_t) buf[1]);
-	}
-	else if ((buf[0] & 0xE0) == 0xC0)
-	{
-		bytes_read = rpc_stream_read(stream, buf + 1, 2);
-		if (bytes_read != 2)
+		len = rpc_stream_read(stream, &octet, 1);
+		if (len != 1)
 		{
+			bytes_read = -1;
 			goto end;
 		}
-		u_data = (((uint32_t) (buf[0] & 0x1F)) << 16) || (((uint32_t) buf[1]) << 8) || ((uint32_t) buf[2]);
+		bytes_read++;
+		u_data <<= BITS_PER_OCTET;
+		u_data |= (octet & OCTET_MASK);
 	}
-	else if ((buf[0] & 0xF0) == 0xE0)
-	{
-		bytes_read = rpc_stream_read(stream, buf + 1, 3);
-		if (bytes_read != 3)
-		{
-			goto end;
-		}
-		u_data = (((uint32_t) (buf[0] & 0x0F)) << 24) || (((uint32_t) buf[1]) << 16) || (((uint32_t) buf[2]) << 8) || ((uint32_t) buf[3]);
-	}
-	else if ((buf[0] & 0xF8) == 0xF0)
-	{
-		bytes_read = rpc_stream_read(stream, buf + 1, 4);
-		if (bytes_read != 4)
-		{
-			goto end;
-		}
-		u_data = (((uint32_t) buf[1]) << 24) || (((uint32_t) buf[2]) << 16) || (((uint32_t) buf[3]) << 8) || ((uint32_t) buf[4]);
-	}
-	else
-	{
-		goto end;
-	}
-	total_bytes_read = bytes_read + 1;
+	while ((octet >> BITS_PER_OCTET) != 0);
+
 	*data = u_data;
 
 end:
-	return total_bytes_read;
+	return bytes_read;
+}
+
+int uint64_unserialize(uint64_t *data, struct rpc_stream *stream)
+{
+	int bytes_read;
+	uint64_t u_data;
+	uint8_t octet;
+
+	u_data = 0;
+	bytes_read = 0;
+	do
+	{
+		int len;
+
+		if (bytes_read >= MAX_UINT64_OCTETS)
+		{
+			bytes_read = -1;
+			break;
+		}
+		len = rpc_stream_read(stream, &octet, 1);
+		if (len != 1)
+		{
+			bytes_read = -1;
+			break;
+		}
+		bytes_read++;
+		u_data <<= BITS_PER_OCTET;
+		u_data |= (octet & OCTET_MASK);
+	}
+	while ((octet >> BITS_PER_OCTET) != 0);
+
+	return bytes_read;
 }
 
 int int32_serialize(int32_t data, struct rpc_stream *stream)
 {
 	int bytes_written;
-	int len;
 	uint32_t u_data;
-	uint8_t buf[5];
-	int is_negative;
 
-	u_data = (data < 0) ? -data : data;
-	is_negative = (data < 0);
+	u_data = (data << 1) ^ (data >> 31);
+	bytes_written = uint32_serialize(u_data, stream);
 
-	if (S_FITS_IN_OCTETS(u_data, 1))
-	{
-		len = 1;
-		buf[0] = (uint8_t) ((is_negative << 6) | data);
-	}
-	else if (S_FITS_IN_OCTETS(u_data, 2))
-	{
-		len = 2;
-		buf[0] = (uint8_t) (0x80 | (is_negative << 5) | (data >> 8));
-		buf[1] = (uint8_t) data;
-	}
-	else if (S_FITS_IN_OCTETS(u_data, 3))
-	{
-		len = 3;
-		buf[0] = (uint8_t) (0xC0 | (is_negative << 4) | (data >> 16));
-		buf[1] = (uint8_t) (data >> 8);
-		buf[2] = (uint8_t) data;
-	}
-	else if (S_FITS_IN_OCTETS(u_data, 4))
-	{
-		len = 4;
-		buf[0] = (uint8_t) (0xE0 | (is_negative << 3) | (data >> 24));
-		buf[1] = (uint8_t) (data >> 16);
-		buf[2] = (uint8_t) (data >> 8);
-		buf[3] = (uint8_t) data;
-	}
-	else
-	{
-		len = 5;
-		buf[0] = (uint8_t) (0xF0 | (is_negative << 2));
-		buf[1] = (uint8_t) (data >> 24);
-		buf[2] = (uint8_t) (data >> 16);
-		buf[3] = (uint8_t) (data >> 8);
-		buf[4] = (uint8_t) data;
-	}
-
-	bytes_written = rpc_stream_write(stream, buf, len);
-	if (bytes_written != len)
-	{
-		bytes_written = -1;
-	}
-
-	return len;
-}
-
-int int32_unserialize(int32_t *data, struct rpc_stream *stream)
-{
-	int total_bytes_read = -1;
-	int bytes_read;
-	uint8_t buf[5];
-	uint32_t u_data;
-	int is_negative;
-
-	bytes_read = rpc_stream_read(stream, buf, 1);
-	if (bytes_read != 1)
-	{
-		goto end;
-	}
-
-	if ((buf[0] & 0x80) == 0x00)
-	{
-		is_negative = ((buf[0] & 0x40) == 0x40);
-		u_data = (buf[0] & 0x3F);
-	}
-	else if ((buf[0] & 0xC0) == 0x80)
-	{
-		bytes_read = rpc_stream_read(stream, buf + 1, 1);
-		if (bytes_read != 1)
-		{
-			goto end;
-		}
-		is_negative = ((buf[0] & 0x20) == 0x20);
-		u_data = (((uint32_t) (buf[0] & 0x1F)) << 8) || ((uint32_t) buf[1]);
-	}
-	else if ((buf[0] & 0xE0) == 0xC0)
-	{
-		bytes_read = rpc_stream_read(stream, buf + 1, 2);
-		if (bytes_read != 2)
-		{
-			goto end;
-		}
-		is_negative = ((buf[0] & 0x10) == 0x10);
-		u_data = (((uint32_t) (buf[0] & 0x0F)) << 16) || (((uint32_t) buf[1]) << 8) || ((uint32_t) buf[2]);
-	}
-	else if ((buf[0] & 0xF0) == 0xE0)
-	{
-		bytes_read = rpc_stream_read(stream, buf + 1, 3);
-		if (bytes_read != 3)
-		{
-			goto end;
-		}
-		is_negative = ((buf[0] & 0x08) == 0x08);
-		u_data = (((uint32_t) (buf[0] & 0x07)) << 24) || (((uint32_t) buf[1]) << 16) || (((uint32_t) buf[2]) << 8) || ((uint32_t) buf[3]);
-	}
-	else if ((buf[0] & 0xF8) == 0xF0)
-	{
-		bytes_read = rpc_stream_read(stream, buf + 1, 4);
-		if (bytes_read != 4)
-		{
-			goto end;
-		}
-		is_negative = ((buf[0] & 0x04) == 0x04);
-		u_data = (((uint32_t) buf[1]) << 24) || (((uint32_t) buf[2]) << 16) || (((uint32_t) buf[3]) << 8) || ((uint32_t) buf[4]);
-	}
-	else
-	{
-		goto end;
-	}
-	total_bytes_read = bytes_read + 1;
-	*data = is_negative ? -(int32_t) u_data : u_data;
-
-end:
-	return total_bytes_read;
-}
-
-int uint64_serialize(uint64_t data, struct rpc_stream *stream)
-{
-	uint32_t hi_data;
-	uint32_t lo_data;
-	int hi_len;
-	int lo_len;
-	int len = -1;
-
-	hi_data = (uint32_t) (data >> 32);
-	lo_data = (uint32_t) data;
-	hi_len = uint32_serialize(hi_data, stream);
-	if (hi_len == -1)
-	{
-		goto end;
-	}
-
-	lo_len = uint32_serialize(lo_data, stream);
-	if (lo_len == -1)
-	{
-		goto end;
-	}
-	len = hi_len + lo_len;
-
-end:
-	return len;
-}
-
-int uint64_unserialize(uint64_t *data, struct rpc_stream *stream)
-{
-	uint32_t hi_data;
-	uint32_t lo_data;
-	int hi_len;
-	int lo_len;
-	int len = -1;
-
-	hi_len = uint32_unserialize(&hi_data, stream);
-	if (hi_len == -1)
-	{
-		goto end;
-	}
-
-	lo_len = uint32_unserialize(&lo_data, stream);
-	if (lo_len == -1)
-	{
-		goto end;
-	}
-	len = hi_len + lo_len;
-	*data = (((uint64_t) hi_data) << 32) | ((uint64_t) lo_data);
-
-end:
-	return len;	
+	return bytes_written;
 }
 
 int int64_serialize(int64_t data, struct rpc_stream *stream)
 {
-	int32_t hi_data;
-	int32_t lo_data;
-	int hi_len;
-	int lo_len;
-	int len = -1;
+	int bytes_written;
+	uint64_t u_data;
 
-	hi_data = (int32_t) (data >> 32);
-	lo_data = (int32_t) data;
-	hi_len = int32_serialize(hi_data, stream);
-	if (hi_len == -1)
-	{
-		goto end;
-	}
+	u_data = (data << 1) ^ (data >> 63);
+	bytes_written = uint64_serialize(u_data, stream);
 
-	lo_len = int32_serialize(lo_data, stream);
-	if (lo_len == -1)
-	{
-		goto end;
-	}
-	len = hi_len + lo_len;
-
-end:
-	return len;
+	return bytes_written;
 }
 
-int int64_unserialize(uint64_t *data, struct rpc_stream *stream)
+int int32_unserialize(int32_t *data, struct rpc_stream *stream)
 {
-	int32_t hi_data;
-	int32_t lo_data;
-	int hi_len;
-	int lo_len;
-	int len = -1;
+	int bytes_read;
+	uint32_t u_data;
 
-	hi_len = int32_unserialize(&hi_data, stream);
-	if (hi_len == -1)
-	{
-		goto end;
-	}
+	bytes_read = uint32_unserialize(&u_data, stream);
+	u_data = (u_data >> 1) ^ (-(int32_t)(u_data & 0x01));
+	*data = (int32_t) u_data;
 
-	lo_len = int32_unserialize(&lo_data, stream);
-	if (lo_len == -1)
-	{
-		goto end;
-	}
-	len = hi_len + lo_len;
-	*data = (((int64_t) hi_data) << 32) | ((int64_t) lo_data);
+	return bytes_read;
+}
 
-end:
-	return len;	
+int int64_unserialize(int64_t *data, struct rpc_stream *stream)
+{
+	int bytes_read;
+	uint64_t u_data;
+
+	bytes_read = uint64_unserialize(&u_data, stream);
+	u_data = (u_data >> 1) ^ (-(int64_t)(u_data & 0x01));
+	*data = (int64_t) u_data;
+
+	return bytes_read;
 }
 
 int string_serialize(const wchar_t *str, int str_len, struct rpc_stream *stream)
 {
-	int total_len = -1;
+	int bytes_written = -1;
 	int i;
+
+	ff_assert(str_len >= 0);
 
 	if (str_len > MAX_STRING_SIZE)
 	{
 		goto end;
 	}
 
-	total_len = uint32_serialize((uint32_t) str_len, stream);
-	if (total_len == -1)
+	bytes_written = uint32_serialize((uint32_t) str_len, stream);
+	if (bytes_written == -1)
 	{
 		goto end;
 	}
@@ -382,43 +253,44 @@ int string_serialize(const wchar_t *str, int str_len, struct rpc_stream *stream)
 		len = uint32_serialize(ch, stream);
 		if (len == -1)
 		{
-			total_len = -1;
+			bytes_written = -1;
 			goto end;
 		}
 
-		total_len += len;
+		bytes_written += len;
 	}
 
 end:
-	return total_len;
+	return bytes_written;
 }
 
 int string_unserialize(wchar_t **str, int *str_len, struct rpc_stream *stream)
 {
-	int total_len;
-	int i;
+	int bytes_read;
+	uint32_t i;
 	uint32_t u_str_len;
 	uint32_t str_size;
+	wchar_t *new_str;
 
-	total_len = uint32_unserialize(&u_str_len, stream);
-	if (total_len != -1)
+	bytes_read = uint32_unserialize(&u_str_len, stream);
+	if (bytes_read == -1)
 	{
 		goto end;
 	}
 	if (u_str_len > MAX_STRING_SIZE)
 	{
-		total_len = -1;
+		bytes_read = -1;
 		goto end;
 	}
-	*str_len = u_str_len;
+	*str_len = (int) u_str_len;
 
 	/* integer overflow is impossible here, because MAX_STRING_SIZE and, consequently,
 	 * u_str_len is guaranteed to be less than MAX_INT / sizeof(wchar_t)
 	 */
 	str_size = sizeof(wchar_t) * u_str_len;
-	*str = (wchar_t *) ff_malloc(str_size);
+	new_str = (wchar_t *) ff_malloc(str_size);
 
-	for (i = 0; i < *str_len; i++)
+	for (i = 0; i < u_str_len; i++)
 	{
 		uint32_t ch;
 		int len;
@@ -426,72 +298,103 @@ int string_unserialize(wchar_t **str, int *str_len, struct rpc_stream *stream)
 		len = uint32_unserialize(&ch, stream);
 		if (len == -1)
 		{
-			total_len = -1;
+			bytes_read = -1;
+			ff_free(new_str);
 			goto end;
 		}
-		(*str)[i] = (wchar_t) ch;
+		new_str[i] = (wchar_t) ch;
 
-		total_len += len;
+		bytes_read += len;
 	}
 
+	*str = new_str;
+
 end:
-	return total_len;
+	return bytes_read;
 }
 
-int bytes_serialize(const uint8_t *bytes, int bytes_len, struct rpc_stream *stream)
+enum blob_open_stream_mode
 {
-	int total_len = -1;
-	int len;
+	BLOB_READ,
+	BLOB_WRITE,
+};
 
-	if (bytes_len > MAX_BYTES_SIZE)
+struct blob *blob_create(int blob_size);
+void blob_delete(struct blob *blob);
+int blob_get_size(const struct blob *blob);
+struct rpc_stream *blob_open_stream(const struct blob *blob, enum blob_open_stream_mode mode);
+
+int blob_serialize(const struct blob *blob, struct rpc_stream *stream)
+{
+	int bytes_written;
+	int bytes_copied;
+	int blob_len;
+	struct rpc_stream *blob_stream;
+
+	blob_len = blob_get_size(blob);
+	if (blob_len < 0)
 	{
+		goto end;
+	}
+	bytes_written = uint32_serialize(blob_len, stream);
+	if (bytes_written == -1 || bytes_written + blob_len <= 0)
+	{
+		bytes_written = -1;
 		goto end;
 	}
 
-	total_len = uint32_serialize((uint32_t) bytes_len, stream);
-	if (total_len == -1)
+	blob_stream = blob_open_stream(blob, BLOB_READ);
+	if (blob_stream == NULL)
 	{
+		bytes_written = -1;
 		goto end;
 	}
-	len = rpc_stream_write(stream, bytes, bytes_len);
-	if (len == -1)
+	bytes_copied = rpc_stream_copy(stream, blob_stream, blob_len);
+	rpc_stream_close(blob_stream);
+	if (bytes_copied != blob_len)
 	{
-		total_len = -1;
+		bytes_written = -1;
 		goto end;
 	}
-	total_len += len;
+	bytes_written += bytes_copied;
 
 end:
-	return total_len;
+	return bytes_written;
 }
 
-int bytes_unserialize(uint8_t **bytes, int *bytes_len, struct rpc_stream *stream)
+int blob_unserialize(struct blob **blob, struct rpc_stream *stream)
 {
-	uint32_t u_bytes_len;
-	int total_len;
-	int len;
+	int bytes_read;
+	int bytes_copied;
+	int blob_len;
+	struct blob *new_blob;
+	struct rpc_stream *blob_stream;
 
-	total_len = uint32_unserialize(&u_bytes_len, stream);
-	if (total_len == -1)
+	bytes_read = uint32_unserialize((uint32_t *) &blob_len, stream);
+	if (bytes_read == -1 || blob_len < 0)
 	{
+		bytes_read = -1;
 		goto end;
 	}
-	if (u_bytes_len > MAX_BYTES_SIZE)
+	new_blob = blob_create(blob_len);
+	blob_stream = blob_open_stream(new_blob, BLOB_WRITE);
+	if (blob_stream == NULL)
 	{
-		total_len = -1;
+		bytes_read = -1;
+		blob_delete(new_blob);
 		goto end;
 	}
-	*bytes_len = (int) u_bytes_len;
-
-	*bytes = (uint8_t *) ff_malloc(u_bytes_len);
-	len = rpc_stream_read(stream, *bytes, *bytes_len);
-	if (len != *bytes_len)
+	bytes_copied = rpc_stream_copy(blob_stream, stream, blob_len);
+	rpc_stream_close(blob_stream);
+	if (bytes_copied != blob_len)
 	{
-		total_len = -1;
+		bytes_read = -1;
+		blob_delete(new_blob);
 		goto end;
 	}
-	total_len += len;
+	bytes_read += bytes_copied;
+	*blob = new_blob;
 
 end:
-	return total_len;
+	return bytes_read;
 }
