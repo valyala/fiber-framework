5e9ed833cbe2290cffd08bdb374ce05a06c1f876
valyala <valyala@6b04b937-504c-0410-a5dc-c58d0effbdb5>
2008-07-17 12:34:17 +0000

    ff_blocking_queue functions made more consistent

--- trunk/src/ff_blocking_queue.c
+++ trunk/src/ff_blocking_queue.c
@@ -33,19 +33,15 @@ void ff_blocking_queue_delete(struct ff_blocking_queue *queue)
 	ff_free(queue);
 }
 
-void *ff_blocking_queue_get(struct ff_blocking_queue *queue)
+void ff_blocking_queue_get(struct ff_blocking_queue *queue, void **data)
 {
-	void *data;
-
 	ff_semaphore_down(queue->producer_semaphore);
-	data = ff_queue_front(queue->simple_queue);
+	*data = ff_queue_front(queue->simple_queue);
 	ff_queue_pop(queue->simple_queue);
 	ff_semaphore_up(queue->consumer_semaphore);
-
-	return data;
 }
 
-int ff_blocking_queue_get_with_timeout(struct ff_blocking_queue *queue, int timeout, void **data)
+int ff_blocking_queue_get_with_timeout(struct ff_blocking_queue *queue, void **data, int timeout)
 {
 	int is_success;
 	
@@ -64,4 +60,17 @@ void ff_blocking_queue_put(struct ff_blocking_queue *queue, void *data)
 	ff_semaphore_down(queue->consumer_semaphore);
 	ff_queue_push(queue->simple_queue, data);
 	ff_semaphore_up(queue->producer_semaphore);
+}
+
+int ff_blocking_queue_put_with_timeout(struct ff_blocking_queue *queue, void *data, int timeout)
+{
+	int is_success;
+
+	is_success = ff_semaphore_down_with_timeout(queue->consumer_semaphore, timeout);
+	if (is_success)
+	{
+		ff_queue_push(queue->simple_queue, data);
+		ff_semaphore_up(queue->producer_semaphore);
+	}
+	return is_success;
 }
