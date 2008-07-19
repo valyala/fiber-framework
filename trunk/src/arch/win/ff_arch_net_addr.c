cbeff345cfa857028b78d33a14ad0b0ba8aa6ac6
valyala <valyala@6b04b937-504c-0410-a5dc-c58d0effbdb5>
2008-07-19 17:11:53 +0000

    added ff_arch_net_addr_get_broadcast_addr() and ff_arch_net_addr_is_equal() functions

--- trunk/src/arch/win/ff_arch_net_addr.c
+++ trunk/src/arch/win/ff_arch_net_addr.c
@@ -80,3 +80,20 @@ int ff_arch_net_addr_resolve(struct ff_arch_net_addr *addr, const wchar_t *host,
 
 	return data.is_success;
 }
+
+void ff_arch_net_addr_get_broadcast_addr(const struct ff_arch_net_addr *addr, const struct ff_arch_net_addr *net_mask, struct ff_arch_net_addr *broadcast_addr)
+{
+	struct sockaddr_in tmp_addr;
+
+	memcpy(&tmp_addr, &addr->addr, sizeof(tmp_addr));
+	tmp_addr.sin_addr.s_addr = addr->addr.sin_addr.s_addr | ~(net_mask->addr.sin_addr.s_addr);
+	memcpy(&broadcast_addr->addr, &tmp_addr, sizeof(tmp_addr));
+}
+
+int ff_arch_net_addr_is_equal(const struct ff_arch_net_addr *addr1, const struct ff_arch_net_addr *addr2)
+{
+	int is_equal;
+	
+	is_equal = (memcmp(&addr1->addr, &addr2->addr, sizeof(addr1->addr)) == 0);
+	return is_equal;
+}
