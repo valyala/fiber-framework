7f9a6f4821cb0a1d912e5bfe4529a97ddf7ba015
valyala <valyala@6b04b937-504c-0410-a5dc-c58d0effbdb5>
2008-07-19 17:11:53 +0000

    added ff_arch_net_addr_get_broadcast_addr() and ff_arch_net_addr_is_equal() functions

--- include/public/arch/ff_arch_net_addr.h
+++ include/public/arch/ff_arch_net_addr.h
@@ -15,6 +15,10 @@ FF_API void ff_arch_net_addr_delete(struct ff_arch_net_addr *addr);
 
 FF_API int ff_arch_net_addr_resolve(struct ff_arch_net_addr *addr, const wchar_t *host, int port);
 
+FF_API void ff_arch_net_addr_get_broadcast_addr(const struct ff_arch_net_addr *addr, const struct ff_arch_net_addr *net_mask, struct ff_arch_net_addr *broadcast_addr);
+
+FF_API int ff_arch_net_addr_is_equal(const struct ff_arch_net_addr *addr1, const struct ff_arch_net_addr *addr2);
+
 #ifdef __cplusplus
 }
 #endif
