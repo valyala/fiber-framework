153593592a24b961a7863d6a0ae27b4db933549d
valyala <valyala@6b04b937-504c-0410-a5dc-c58d0effbdb5>
2008-08-19 14:04:04 +0000

    added missing file

---
+++ src/arch/linux/ff_linux_error_check.c
@@ -0,0 +1,23 @@
+#include "private/ff_common.h"
+
+#include "ff_linux_error_check.h"
+#include "ff_linux_misc.h"
+
+#include <stdarg.h>
+
+void fatal_error(const wchar_t *format, ...)
+{
+	int len;
+	char *mb_format;
+
+	mb_format = ff_linux_misc_wide_to_multibyte_string(format);
+
+	va_list args_ptr;
+	va_start(args_ptr, format);
+	len = fprintf(stderr, mb_format, args_ptr);
+	ff_assert(len >= 0);
+	va_end(args_ptr);
+
+	ff_free(mb_format);
+	exit(EXIT_FAILURE);
+}
