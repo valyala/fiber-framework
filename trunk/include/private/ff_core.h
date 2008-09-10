9a123ef0979d059ab36c32faf1547ed846d9fe4b
valyala <valyala@6b04b937-504c-0410-a5dc-c58d0effbdb5>
2008-09-10 09:09:09 +0000

    updated comments for private ff_core.h

--- trunk/include/private/ff_core.h
+++ trunk/include/private/ff_core.h
@@ -11,22 +11,26 @@ extern "C" {
 
 /**
  * @public
- * Schedules the given fiber for execution
+ * Schedules the given fiber for execution.
  */
 void ff_core_schedule_fiber(struct ff_fiber *fiber);
 
 /**
  * @public
- * Yields the current fiber
+ * Yields the current fiber.
+ * The fiber should be scheduled explicitly using ff_core_schedule_fiber()
+ * after it has been yielded. Otherwise it will never execute again.
  */
 void ff_core_yield_fiber();
 
 /**
  * @public
- * the function, which is called when cancelling the timed out operation
+ * the function, which is called when cancelling the timed out operation.
+ * This function shouldn't block - all blocking operations should be executed
+ * asynchronously using the ff_core_fiberpool_execute_async() function.
+ * This function shouldn't call the ff_core_deregister_timeout_operation() in order to avoid deadlocks.
  */
 typedef void (*ff_core_cancel_timeout_func)(struct ff_fiber *fiber, void *ctx);
-
 
 /**
  * @public
@@ -46,6 +50,8 @@ struct ff_core_timeout_operation_data *ff_core_register_timeout_operation(int ti
  * @public
  * Deregisters the timeout operation, which was registered using ff_core_register_timeout_operation()
  * Returns 1 on success, 0 on error.
+ * Usually this function is called in the same function as the ff_core_register_timeout_operation()
+ * after completing the given operation.
  */
 int ff_core_deregister_timeout_operation(struct ff_core_timeout_operation_data *timeout_operation_data);
 
