0f5ab329d23bf700d35746411e1d28615f6833ef
valyala <valyala@6b04b937-504c-0410-a5dc-c58d0effbdb5>
2008-10-02 13:47:42 +0000

    added ff_stream based on tcp with configurable read and write timeouts

---
+++ trunk/include/private/ff_stream_tcp_with_timeout.h
@@ -0,0 +1,14 @@
+#ifndef FF_STREAM_TCP_WITH_TIMEOUT_PRIVATE
+#define FF_STREAM_TCP_WITH_TIMEOUT_PRIVATE
+
+#include "ff/ff_stream_tcp_with_timeout.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
