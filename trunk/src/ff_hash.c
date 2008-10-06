1f33f4f90774d3707a07c5c33cb5d4bdd88273dc
valyala <valyala@6b04b937-504c-0410-a5dc-c58d0effbdb5>
2008-10-06 08:46:17 +0000

    ff_hash() function was renamed to the ff_hash_uint32(); added ff_hash_uint8() and ff_hash_uint16() functions

--- trunk/src/ff_hash.c
+++ trunk/src/ff_hash.c
@@ -32,9 +32,11 @@
  * and gives distribution close to uniform for typical input.
  * See http://burtleburtle.net/bob/c/lookup3.c for reference implementation.
  */
-uint32_t ff_hash(uint32_t start_value, const uint32_t *buf, int buf_size)
+uint32_t ff_hash_uint32(uint32_t start_value, const uint32_t *buf, int buf_size)
 {
 	uint32_t a, b, c;
+
+	ff_assert(buf_size >= 0);
 
 	a = b = c = 0xdeadbeef + (((uint32_t) buf_size) << 2) + start_value;
 	while (buf_size > 3)
@@ -58,4 +60,58 @@ uint32_t ff_hash(uint32_t start_value, const uint32_t *buf, int buf_size)
 	}
 
 	return c;
+}
+
+uint32_t ff_hash_uint16(uint32_t start_value, const uint16_t *buf, int buf_size)
+{
+	int uint32_chunks_cnt;
+	uint32_t hash;
+	int tail_size;
+
+	ff_assert(buf_size >= 0);
+	uint32_chunks_cnt = buf_size >> 1;
+	hash = ff_hash_uint32(start_value, (const uint32_t *) buf, uint32_chunks_cnt);
+	tail_size = buf_size & 0x01;
+	if (tail_size == 1)
+	{
+		uint32_t tail_uint32;
+		const uint16_t *tail;
+
+		tail = buf + (uint32_chunks_cnt << 1);
+		tail_uint32 = (uint32_t) tail[0];
+		hash = ff_hash_uint32(hash, &tail_uint32, 1);
+	}
+	return hash;
+}
+
+uint32_t ff_hash_uint8(uint32_t start_value, const uint8_t *buf, int buf_size)
+{
+	int uint32_chunks_cnt;
+	uint32_t hash;
+	int tail_size;
+
+	ff_assert(buf_size >= 0);
+
+	uint32_chunks_cnt = buf_size >> 2;
+	hash = ff_hash_uint32(start_value, (const uint32_t *) buf, uint32_chunks_cnt);
+	tail_size = buf_size & 0x03;
+	if (tail_size > 0)
+	{
+		uint32_t tail_uint32;
+		const uint8_t *tail;
+
+		tail = buf + (uint32_chunks_cnt << 2);
+		switch (tail_size)
+		{
+			case 3:
+				tail_uint32 = (((uint32_t) tail[0]) << 16) | (((uint32_t) tail[1]) << 8) | ((uint32_t) tail[2]);
+				break;
+			case 2:
+				tail_uint32 = (((uint32_t) tail[0]) << 8) | ((uint32_t) tail[1]);
+			case 1:
+				tail_uint32 = (uint32_t) tail[0];
+		}
+		hash = ff_hash_uint32(hash, &tail_uint32, 1);
+	}
+	return hash;
 }
