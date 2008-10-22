211e9abb51ac8026ddf5bdf51b87dc06c4521ffa
valyala <valyala@6b04b937-504c-0410-a5dc-c58d0effbdb5>
2008-10-22 08:12:37 +0000

    ff_malloc.h made public

---
+++ trunk/include/ff/ff_api.h
@@ -0,0 +1,12 @@
+#ifndef FF_API_PUBLIC
+#define FF_API_PUBLIC
+
+#if defined(FF_BUILD_DLL)
+	#define FF_API __declspec(dllexport)
+#elif defined(FF_USE_DLL)
+	#define FF_API __declspec(dllimport)
+#else
+	#define FF_API extern
+#endif
+
+#endif
