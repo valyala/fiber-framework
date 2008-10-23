86b3ccaf6988c5ff5f3f6efa47dbf11a08b75ad0
valyala <valyala@6b04b937-504c-0410-a5dc-c58d0effbdb5>
2008-10-23 08:03:30 +0000

    added ff_endpoint

---
+++ include/private/ff_endpoint.h
@@ -0,0 +1,15 @@
+#ifndef FF_ENDPOINT_PRIVATE
+#define FF_ENDPOINT_PRIVATE
+
+#include "ff/ff_endpoint.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/ff_endpoint.c
@@ -0,0 +1,45 @@
+#include "private/ff_common.h"
+
+#include "private/ff_endpoint.h"
+#include "private/ff_stream.h"
+
+struct ff_endpoint
+{
+	struct ff_endpoint_vtable *vtable;
+	void *ctx;
+};
+
+struct ff_endpoint *ff_endpoint_create(struct ff_endpoint_vtable *vtable, void *ctx)
+{
+	struct ff_endpoint *endpoint;
+
+	endpoint = (struct ff_endpoint *) ff_malloc(sizeof(*endpoint));
+	endpoint->vtable = vtable;
+	endpoint->ctx = ctx;
+
+	return endpoint;
+}
+
+void ff_endpoint_delete(struct ff_endpoint *endpoint)
+{
+	endpoint->vtable->delete(endpoint);
+	ff_free(endpoint);
+}
+
+void *ff_endpoint_get_ctx(struct ff_endpoint *endpoint)
+{
+	return endpoint->ctx;
+}
+
+struct ff_stream *ff_endpoint_accept(struct ff_endpoint *endpoint)
+{
+	struct ff_stream *stream;
+
+	stream = endpoint->vtable->accept(endpoint);
+	return stream;
+}
+
+void ff_endpoint_disconnect(struct ff_endpoint *endpoint)
+{
+	endpoint->vtable->disconnect(endpoint);
+}
