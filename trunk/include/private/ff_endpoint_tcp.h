4471ff9a0fdd49844a5b9e0681ca15de551d006a
valyala <valyala@6b04b937-504c-0410-a5dc-c58d0effbdb5>
2008-10-23 08:51:12 +0000

    added ff_endpoint_tcp

---
+++ trunk/include/private/ff_endpoint_tcp.h
@@ -0,0 +1,15 @@
+#ifndef FF_ENDPOINT_TCP_PRIVATE
+#define FF_ENDPOINT_TCP_PRIVATE
+
+#include "ff/ff_endpoint_tcp.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
