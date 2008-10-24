a6c8f746a493fbdfcd5127209b0a2d60c4a559cd
valyala <valyala@6b04b937-504c-0410-a5dc-c58d0effbdb5>
2008-10-24 16:52:17 +0000

    added ff_stream_connector

---
+++ trunk/include/ff/ff_stream_connector.h
@@ -0,0 +1,31 @@
+#ifndef FF_STREAM_CONNECTOR_PUBLIC
+#define FF_STREAM_CONNECTOR_PUBLIC
+
+#include "ff/ff_common.h"
+#include "ff/ff_stream.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct ff_stream_connector;
+
+struct ff_stream_connector_vtable
+{
+	void (*delete)(struct ff_stream_connector *stream_connector);
+	struct ff_stream *(*connect)(struct ff_stream_connector *stream_connector);
+};
+
+FF_API struct ff_stream_connector *ff_stream_connector_create(const ff_stream_connector_vtable *vtable, void *ctx);
+
+FF_API void ff_stream_connector_delete(struct ff_stream_connector *stream_connector);
+
+FF_API void *ff_stream_connector_get_ctx(struct ff_stream_connector *stream_connector);
+
+FF_API struct ff_stream *ff_stream_connector_connect(struct ff_stream_connector *stream_connector);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ trunk/include/private/ff_stream_connector.h
@@ -0,0 +1,15 @@
+#ifndef FF_STREAM_CONNECTOR_PRIVATE
+#define FF_STREAM_CONNECTOR_PRIVATE
+
+#include "ff/ff_stream_connector.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
