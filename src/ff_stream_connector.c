e93af289a215c0a77376567a88b04a7eb72d4ccd
valyala <valyala@6b04b937-504c-0410-a5dc-c58d0effbdb5>
2008-10-24 16:52:17 +0000

    added ff_stream_connector

---
+++ src/ff_stream_connector.c
@@ -0,0 +1,40 @@
+#include "private/ff_common.h"
+
+#include "private/stream_connector.h"
+#include "private/ff_stream.h"
+
+struct ff_stream_connector
+{
+	const ff_stream_connector_vtable *vtable;
+	void *ctx;
+};
+
+struct ff_stream_connector *ff_stream_connector_create(const ff_stream_connector_vtable *vtable, void *ctx)
+{
+	struct ff_stream_connector *stream_connector;
+
+	stream_connector = (struct ff_stream_connector *) ff_malloc(sizeof(*stream_connector));
+	stream_connector->vtable = vtable;
+	stream_connector->ctx = ctx;
+
+	return stream_connector;
+}
+
+void ff_stream_connector_delete(struct ff_stream_connector *stream_connector)
+{
+	stream_connector->vtable->delete(stream_connector);
+	ff_free(stream_connector);
+}
+
+void *ff_stream_connector_get_ctx(struct ff_stream_connector *stream_connector)
+{
+	return stream_connector->ctx;
+}
+
+struct ff_stream *ff_stream_connector_connect(struct ff_stream_connector *stream_connector)
+{
+	struct ff_stream *stream;
+
+	stream = stream_connector->vtable->connect(stream_connector);
+	return stream;
+}
