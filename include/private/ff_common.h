5d906ebaa512891205a6e9b0a85c6ba81731e080
valyala <valyala@6b04b937-504c-0410-a5dc-c58d0effbdb5>
2008-11-06 11:49:00 +0000

    removed FF_BUILD_DLL and FF_USE_DLL macro requirements from the public ff_api.h

--- include/private/ff_common.h
+++ include/private/ff_common.h
@@ -1,10 +1,11 @@
 #ifndef FF_COMMON_PRIVATE
 #define FF_COMMON_PRIVATE
 
+#include "private/ff_api.h"
+#include "ff/ff_common.h"
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-
-#include "ff/ff_common.h"
 
 #endif
