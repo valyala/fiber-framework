cf9d70e60929f722f53da5e66b5e272b0606b610
valyala <valyala@6b04b937-504c-0410-a5dc-c58d0effbdb5>
2008-11-06 12:16:59 +0000

    fixed wrong public FF_API definition for gcc

--- include/private/ff_api.h
+++ include/private/ff_api.h
@@ -7,13 +7,9 @@
 	#error ff/ff_api.h file must define FF_API
 #endif
 
-/* undefine public definition of the FF_API */
-#undef FF_API
-
 #if defined(WIN32)
+	#undef FF_API
 	#define FF_API __declspec(dllexport)
-#else
-	#define FF_API __attribute__((visibility("default")))
 #endif
 
 #endif
