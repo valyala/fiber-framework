74b9e38e459a7f544b20851c0886eca16e32f9c1
valyala <valyala@6b04b937-504c-0410-a5dc-c58d0effbdb5>
2008-11-12 13:44:39 +0000

    added ff_log_debug() to the ff_tcp.c

--- src/ff_tcp.c
+++ src/ff_tcp.c
@@ -26,6 +26,10 @@ static int tcp_read_func(void *ctx, void *buf, int len)
 
 	tcp = (struct ff_tcp *) ctx;
 	bytes_read = ff_arch_tcp_read(tcp->tcp, buf, len);
+	if (bytes_read == -1)
+	{
+		ff_log_debug(L"error while reading from the tcp=%p to the buf=%p, len=%d. See previous messages for more info", tcp, buf, len);
+	}
 	return bytes_read;
 }
 
@@ -38,6 +42,10 @@ static int tcp_write_func(void *ctx, const void *buf, int len)
 
 	tcp = (struct ff_tcp *) ctx;
 	bytes_written = ff_arch_tcp_write(tcp->tcp, buf, len);
+	if (bytes_written == -1)
+	{
+		ff_log_debug(L"error while writing to the tcp=%p from the buf=%p, len=%d. See previous messages for more info", tcp, buf, len);
+	}
 	return bytes_written;
 }
 
@@ -90,6 +98,10 @@ enum ff_result ff_tcp_bind(struct ff_tcp *tcp, const struct ff_arch_net_addr *ad
 
 	is_listening = ((type == FF_TCP_SERVER) ? 1 : 0);
 	result = ff_arch_tcp_bind(tcp->tcp, addr, is_listening);
+	if (result != FF_SUCCESS)
+	{
+		ff_log_debug(L"cannot bind the tcp=%p to the addr=%p, is_listening=%d. See previous messages for more info", tcp, addr, is_listening);
+	}
 	if (result == FF_SUCCESS && is_listening)
 	{
 		tcp->is_active = 1;
@@ -108,6 +120,10 @@ enum ff_result ff_tcp_connect(struct ff_tcp *tcp, const struct ff_arch_net_addr 
 	{
 		tcp->is_active = 1;
 	}
+	else
+	{
+		ff_log_debug(L"cannot connect tcp=%p to the addr=%p. See previous messages for more info", tcp, addr);
+	}
 	return result;
 }
 
@@ -124,6 +140,14 @@ struct ff_tcp *ff_tcp_accept(struct ff_tcp *tcp, struct ff_arch_net_addr *remote
 			remote_tcp = create_from_arch_tcp(remote_arch_tcp);
 			remote_tcp->is_active = 1;
 		}
+		else
+		{
+			ff_log_debug(L"error while accepting connection on the tcp=%p, remote_addr=%p. See previous messages for more info", tcp, remote_addr);
+		}
+	}
+	else
+	{
+		ff_log_debug(L"the tcp=%p was already disconnected, so it cannot be used for accepting new connections", tcp);
 	}
 	return remote_tcp;
 }
@@ -137,6 +161,14 @@ enum ff_result ff_tcp_read(struct ff_tcp *tcp, void *buf, int len)
 	if (tcp->is_active)
 	{
 		result = ff_read_stream_buffer_read(tcp->read_buffer, buf, len);
+		if (result != FF_SUCCESS)
+		{
+			ff_log_debug(L"error while reading data from the read_buffer=%p to buf=%p, len=%d. See previous messages for more info", tcp->read_buffer, buf, len);
+		}
+	}
+	else
+	{
+		ff_log_debug(L"the tcp=%p was already disconnected, so it cannot be used for reading data to the buf=%p, len=%d", tcp, buf, len);
 	}
 	return result;
 }
@@ -145,13 +177,22 @@ enum ff_result ff_tcp_read_with_timeout(struct ff_tcp *tcp, void *buf, int len, 
 {
 	struct ff_core_timeout_operation_data *timeout_operation_data;
 	enum ff_result result;
+	enum ff_result tmp_result;
 
 	ff_assert(len >= 0);
 	ff_assert(timeout > 0);
 
 	timeout_operation_data = ff_core_register_timeout_operation(timeout, cancel_tcp_operation, tcp);
 	result = ff_tcp_read(tcp, buf, len);
-	ff_core_deregister_timeout_operation(timeout_operation_data);
+	if (result != FF_SUCCESS)
+	{
+		ff_log_debug(L"error while reading data from the tcp=%p to the buf=%p, len=%d using timeout=%d. See previous messages for more info", tcp, buf, len, timeout);
+	}
+	tmp_result = ff_core_deregister_timeout_operation(timeout_operation_data);
+	if (tmp_result != FF_SUCCESS)
+	{
+		ff_log_debug(L"timeout=%d has been exceeded for read operation from the tcp=%p to the buf=%p, len=%d", timeout, tcp, buf, len);
+	}
 
 	return result;
 }
@@ -165,6 +206,14 @@ enum ff_result ff_tcp_write(struct ff_tcp *tcp, const void *buf, int len)
 	if (tcp->is_active)
 	{
 		result = ff_write_stream_buffer_write(tcp->write_buffer, buf, len);
+		if (result != FF_SUCCESS)
+		{
+			ff_log_debug(L"error while writing data to the write_buffer=%p from the buf=%p, len=%d. See previous messages for more info", tcp->write_buffer, buf, len);
+		}
+	}
+	else
+	{
+		ff_log_debug(L"the tcp=%p was already disconnected, so it cannot be used for writing data from the buf=%p, len=%d", tcp, buf, len);
 	}
 	return result;
 }
@@ -173,13 +222,22 @@ enum ff_result ff_tcp_write_with_timeout(struct ff_tcp *tcp, const void *buf, in
 {
 	struct ff_core_timeout_operation_data *timeout_operation_data;
 	enum ff_result result;
+	enum ff_result tmp_result;
 
 	ff_assert(len >= 0);
 	ff_assert(timeout > 0);
 
 	timeout_operation_data = ff_core_register_timeout_operation(timeout, cancel_tcp_operation, tcp);
 	result = ff_tcp_write(tcp, buf, len);
-	ff_core_deregister_timeout_operation(timeout_operation_data);
+	if (result != FF_SUCCESS)
+	{
+		ff_log_debug(L"error while writing data to the tcp=%p from the buf=%p, len=%d using timeout=%d. See previous messages for more info", tcp, buf, len, timeout);
+	}
+	tmp_result = ff_core_deregister_timeout_operation(timeout_operation_data);
+	if (tmp_result != FF_SUCCESS)
+	{
+		ff_log_debug(L"timeout=%d has been exceeded for write operation to the tcp=%p from the buf=%p, len=%d", timeout, tcp, buf, len);
+	}
 
 	return result;
 }
@@ -191,6 +249,14 @@ enum ff_result ff_tcp_flush(struct ff_tcp *tcp)
 	if (tcp->is_active)
 	{
 		result = ff_write_stream_buffer_flush(tcp->write_buffer);
+		if (result != FF_SUCCESS)
+		{
+			ff_log_debug(L"error while flushing the write_buffer=%p. See previous messages for more info", tcp->write_buffer);
+		}
+	}
+	else
+	{
+		ff_log_debug(L"the tcp=%p was already disconnected, so it cannot be flushed", tcp);
 	}
 	return result;
 }
@@ -199,12 +265,21 @@ enum ff_result ff_tcp_flush_with_timeout(struct ff_tcp *tcp, int timeout)
 {
 	struct ff_core_timeout_operation_data *timeout_operation_data;
 	enum ff_result result;
+	enum ff_result tmp_result;
 
 	ff_assert(timeout > 0);
 
 	timeout_operation_data = ff_core_register_timeout_operation(timeout, cancel_tcp_operation, tcp);
 	result = ff_tcp_flush(tcp);
-	ff_core_deregister_timeout_operation(timeout_operation_data);
+	if (result != FF_SUCCESS)
+	{
+		ff_log_debug(L"error while flushing the tcp=%p using timeout=%d. See previous messages for more info", tcp, timeout);
+	}
+	tmp_result = ff_core_deregister_timeout_operation(timeout_operation_data);
+	if (tmp_result != FF_SUCCESS)
+	{
+		ff_log_debug(L"timeout=%d has been exceeded for flush operation on the tcp=%p", timeout, tcp);
+	}
 
 	return result;
 }
@@ -215,5 +290,9 @@ void ff_tcp_disconnect(struct ff_tcp *tcp)
 	{
 		tcp->is_active = 0;
 		ff_arch_tcp_disconnect(tcp->tcp);
+	}
+	else
+	{
+		ff_log_debug(L"the tcp=%p was already disconnected, so it won't be disconnected again", tcp);
 	}
 }
