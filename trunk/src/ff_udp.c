fb145418a9a3e29a07677a9d8e2922799459e8e4
valyala <valyala@6b04b937-504c-0410-a5dc-c58d0effbdb5>
2008-11-12 13:54:56 +0000

    added ff_log_debug() to the ff_udp.c

--- trunk/src/ff_udp.c
+++ trunk/src/ff_udp.c
@@ -40,6 +40,10 @@ enum ff_result ff_udp_bind(struct ff_udp *udp, const struct ff_arch_net_addr *ad
 	enum ff_result result;
 
 	result = ff_arch_udp_bind(udp->udp, addr);
+	if (result != FF_SUCCESS)
+	{
+		ff_log_debug(L"cannot bind the udp=%p to the addr=%p. See previous messages for more info", udp, addr);
+	}
 	return result;
 }
 
@@ -50,12 +54,17 @@ int ff_udp_read(struct ff_udp *udp, struct ff_arch_net_addr *peer_addr, void *bu
 	ff_assert(len >= 0);
 
 	bytes_read = ff_arch_udp_read(udp->udp, peer_addr, buf, len);
+	if (bytes_read == -1)
+	{
+		ff_log_debug(L"error while reading data from the udp=%p into the buf=%p, len=%d, peer_addr=%p. See previous messages for more info", udp, buf, len, peer_addr);
+	}
 	return bytes_read;
 }
 
 int ff_udp_read_with_timeout(struct ff_udp *udp, struct ff_arch_net_addr *peer_addr, void *buf, int len, int timeout)
 {
 	struct ff_core_timeout_operation_data *timeout_operation_data;
+	enum ff_result result;
 	int bytes_read;
 
 	ff_assert(len >= 0);
@@ -63,7 +72,16 @@ int ff_udp_read_with_timeout(struct ff_udp *udp, struct ff_arch_net_addr *peer_a
 
 	timeout_operation_data = ff_core_register_timeout_operation(timeout, cancel_udp_operation, udp);
 	bytes_read = ff_udp_read(udp, peer_addr, buf, len);
-	ff_core_deregister_timeout_operation(timeout_operation_data);
+	if (bytes_read == -1)
+	{
+		ff_log_debug(L"error while reading data from the udp=%p into the buf=%p, len=%d, peer_addr=%p using timeout=%d. See previous messages for more info",
+			udp, buf, len, peer_addr, timeout);
+	}
+	result = ff_core_deregister_timeout_operation(timeout_operation_data);
+	if (result != FF_SUCCESS)
+	{
+		ff_log_debug(L"timeout=%d has been expired on read operation from the udp=%p into the buf=%p, len=%d, peer_addr=%p", timeout, udp, buf, len, peer_addr);
+	}
 
 	return bytes_read;
 }
@@ -75,12 +93,17 @@ int ff_udp_write(struct ff_udp *udp, const struct ff_arch_net_addr *addr, const 
 	ff_assert(len >= 0);
 
 	bytes_written = ff_arch_udp_write(udp->udp, addr, buf, len);
+	if (bytes_written == -1)
+	{
+		ff_log_debug(L"error while writing data to the udp=%p from the buf=%p, len=%d, addr=%p. See previous messages for more info", udp, buf, len, addr);
+	}
 	return bytes_written;
 }
 
 int ff_udp_write_with_timeout(struct ff_udp *udp, const struct ff_arch_net_addr *addr, const void *buf, int len, int timeout)
 {
 	struct ff_core_timeout_operation_data *timeout_operation_data;
+	enum ff_result result;
 	int bytes_written;
 
 	ff_assert(len >= 0);
@@ -88,7 +111,16 @@ int ff_udp_write_with_timeout(struct ff_udp *udp, const struct ff_arch_net_addr 
 
 	timeout_operation_data = ff_core_register_timeout_operation(timeout, cancel_udp_operation, udp);
 	bytes_written = ff_udp_write(udp, addr, buf, len);
-	ff_core_deregister_timeout_operation(timeout_operation_data);
+	if (bytes_written == -1)
+	{
+		ff_log_debug(L"error while writing data to the udp=%p from the buf=%p, len=%d, addr=%p using timeout=%d. See prevsious messages for more info",
+			udp, buf, len, addr, timeout);
+	}
+	result = ff_core_deregister_timeout_operation(timeout_operation_data);
+	if (result != FF_SUCCESS)
+	{
+		ff_log_debug(L"timeout=%d has been expired on write operation to the udp=%p from the buf=%p, len=%d, addr=%p", timeout, udp, buf, len, addr);
+	}
 
 	return bytes_written;
 }
