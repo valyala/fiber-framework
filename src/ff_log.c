af5b5baa8215622dabb70d654b81ddee781326b5
valyala <valyala@6b04b937-504c-0410-a5dc-c58d0effbdb5>
2008-11-12 14:42:23 +0000

    now ff_log_debug() is a macro, which allows to transparently add additional data to the debug message (file name, function name, line, etc.)

--- src/ff_log.c
+++ src/ff_log.c
@@ -53,12 +53,7 @@ void ff_log_shutdown()
 	is_log_initialized = 0;
 }
 
-#if defined(NDEBUG)
-	/* delete ff_log_debug() macro, which is defined in the ff_log.h if the NDEBUG is defined */
-	#undef ff_log_debug
-#endif
-
-void ff_log_debug(const wchar_t *format, ...)
+void ff_log_debug_private(const wchar_t *format, ...)
 {
 	va_list args_ptr;
 
