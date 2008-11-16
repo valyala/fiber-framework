97f5a477c6b6003dacf15768b05672dc1c49c388
valyala <valyala@6b04b937-504c-0410-a5dc-c58d0effbdb5>
2008-11-16 20:30:19 +0000

    removed unmutable ff_mutex::pending_fibers dereference from cycles

--- src/ff_mutex.c
+++ src/ff_mutex.c
@@ -31,12 +31,15 @@ void ff_mutex_delete(struct ff_mutex *mutex)
 
 void ff_mutex_lock(struct ff_mutex *mutex)
 {
+	struct ff_stack *pending_fibers;
+
+	pending_fibers = mutex->pending_fibers;
 	while (mutex->is_locked)
 	{
 		struct ff_fiber *current_fiber;
 
 		current_fiber = ff_fiber_get_current();
-		ff_stack_push(mutex->pending_fibers, current_fiber);
+		ff_stack_push(pending_fibers, current_fiber);
 		ff_core_yield_fiber();
 	}
 	mutex->is_locked = 1;
@@ -44,17 +47,18 @@ void ff_mutex_lock(struct ff_mutex *mutex)
 
 void ff_mutex_unlock(struct ff_mutex *mutex)
 {
+	struct ff_stack *pending_fibers;
 	int is_empty;
 
 	ff_assert(mutex->is_locked);
-
-	is_empty = ff_stack_is_empty(mutex->pending_fibers);
+	pending_fibers = mutex->pending_fibers;
+	is_empty = ff_stack_is_empty(pending_fibers);
 	if (!is_empty)
 	{
 		struct ff_fiber *fiber;
 
-		ff_stack_top(mutex->pending_fibers, (const void **) &fiber);
-		ff_stack_pop(mutex->pending_fibers);
+		ff_stack_top(pending_fibers, (const void **) &fiber);
+		ff_stack_pop(pending_fibers);
 		ff_core_schedule_fiber(fiber);
 	}
 	mutex->is_locked = 0;
